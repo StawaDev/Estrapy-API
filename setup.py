bf614b6942f64acf5de1fa2a0896f5b08eac9895
unknown <damodaraswarupa@gmail.com>
2021-11-25 15:06:18 +0800

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
 from setuptools import setup, find_packages
 
+a = open("README.md", "r")
+READMEFILE = a.read()
+
 setup(
     name="Estrapy-API",
     version="0.1.1",
     packages=find_packages(),
     install_requires=["requests"],
+    license="MIT",
+    url="https://github.com/StawaDev/Estrapy-API",
+    description="A Basic Anime Image API Created By Stawa",
+    long_description=READMEFILE,
+    long_description_content_type="text/markdown",
+    keywords=[
+        "python api",
+        "anime",
+        "anime gif",
+        "roleplay gif"
+    ]
 )
