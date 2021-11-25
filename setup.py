e677c00615f5cfdb02ee5590d9401b1a6e41bf7e
unknown <damodaraswarupa@gmail.com>
2021-11-25 15:43:58 +0800

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-with open('README.md', encoding='utf-8') as readme_file:
-    readme = readme_file.read()
+f = open("README.md", "r")
+README = f.read()
 
 setup(
     name="Estrapy-API",
@@ -10,8 +10,9 @@ setup(
     install_requires=["requests"],
     license="MIT",
     url="https://github.com/StawaDev/Estrapy-API",
+    author="StawaDev",
     description="A Basic Anime Image API Created By Stawa",
-    long_description=readme,
+    long_description=README,
     long_description_content_type="text/markdown",
     keywords=[
         "python api",
