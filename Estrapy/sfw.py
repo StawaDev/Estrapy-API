1feae30d371be2b9b05a49ebf4ccc84a5376ec5e
unknown <damodaraswarupa@gmail.com>
2021-11-25 20:39:43 +0800

    Version 0.1.5

--- Estrapy/sfw.py
+++ Estrapy/sfw.py
@@ -6,24 +6,25 @@ def main_api(user_requests):
     get_api = requests.get(
         f"https://estra-api.herokuapp.com/api/sfw/{user_requests}"
     ).json()
-    return get_api
+    api_dumps = json.dumps(get_api)
+    return api_dumps
 
 
 def run():
-    run_link = json.loads(main_api("run"))
+    run_link = json.loads(main_api("run"))["link"]
     return run_link
 
 
 def hug():
-    hug_link = json.loads(main_api("hug"))
+    hug_link = json.loads(main_api("hug"))["link"]
     return hug_link
 
 
 def smile():
-    smile_link = json.loads(main_api("smile"))
+    smile_link = json.loads(main_api("smile"))["link"]
     return smile_link
 
 
 def headpat():
-    headpat_link = json.loads(main_api("headpat"))
+    headpat_link = json.loads(main_api("headpat")["link"])
     return headpat_link
