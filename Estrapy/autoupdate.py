cc4218a9724b8a31f557626b3f7922f267d50213
unknown <damodaraswarupa@gmail.com>
2021-11-27 20:23:59 +0800

    Version Update

--- Estrapy/autoupdate.py
+++ Estrapy/autoupdate.py
@@ -5,7 +5,7 @@ version = requests.get("https://estra-api.herokuapp.com/").json()["Version"]
 
 
 def autoupdate():
-    if version != "0.1.5":
+    if version != "0.1.6":
         print(
             f"Estrapy-API - new version is up! AutoUpdate will be upgrading version to {version}"
         )
