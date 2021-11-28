3f3d4c56c5e5e452a722a85cc26b1f76840dabcf
adam7100 <73548219+adam757521@users.noreply.github.com>
2021-11-28 14:47:16 +0200

    Code cleanup and errors

--- Estrapy/nsfw.py
+++ Estrapy/nsfw.py
@@ -1,43 +1,17 @@
-import requests
-import json
+from .http import get_api
+
+__all__ = ("Nsfw",)
 
 
-def main_api(user_requests):
-    try:
-        get_api = requests.get(
-            f"https://estra-api.herokuapp.com/api/sfw/{user_requests}"
-        ).json()
-        if get_api != "<Response [503]>":
-            api_dumps = json.dumps(get_api)
-            return api_dumps
-    except:
-        api_down = (
-            "We might have a problem with the API, Please wait for it to be available"
-        )
-        return api_down
+class Nsfw:
+    @staticmethod
+    def kill():
+        return get_api("nsfw/kill")["link"]
 
-
-class nsfw:
-    def kill():  # Not Available Yet!
-        try:
-            kill_link = json.loads(main_api("kill"))["link"]
-            return kill_link
-        except:
-            api_down = "We might have a problem with the API, Please wait for it to be available"
-            return api_down
-
+    @staticmethod
     def yuri():
-        try:
-            yuri_link = json.loads(main_api("kill"))["link"]
-            return yuri_link
-        except:
-            api_down = "We might have a problem with the API, Please wait for it to be available"
-            return api_down
+        return get_api("nsfw/yuri")["link"]
 
+    @staticmethod
     def yaoi():
-        try:
-            yaoi_link = json.loads(main_api("kill"))["link"]
-            return yaoi_link
-        except:
-            api_down = "We might have a problem with the API, Please wait for it to be available"
-            return api_down
+        return get_api("nsfw/yaoi")["link"]
