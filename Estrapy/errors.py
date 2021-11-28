38049a37cdc17ae8ead9f9b1a493e77d4346ef02
unknown <damodaraswarupa@gmail.com>
2021-11-28 19:56:43 +0800

    Added Errors Handler

---
+++ Estrapy/errors.py
@@ -0,0 +1,6 @@
+def errors():
+    "An Function Error That Will Be Used For Handle Error From API Server Down"
+    global api_down
+    api_down = (
+        "We might have a problem with the API, Please wait for it to be available"
+    )
--- Estrapy/sfw.py
+++ Estrapy/sfw.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from errors import *
 
 
 def main_api(user_requests):
@@ -11,43 +12,40 @@ def main_api(user_requests):
             api_dumps = json.dumps(get_api)
             return api_dumps
     except:
-        api_down = (
-            "We might have a problem with the API, Please wait for it to be available"
-        )
         return api_down
 
 
 class sfw:
     def run():
+        "A Function That Will Return a SFW Run GIF"
         try:
             run_link = json.loads(main_api("run"))["link"]
             return run_link
         except:
-            api_down = "We might have a problem with the API, Please wait for it to be available"
             return api_down
 
     def hug():
+        "A Function That Will Return a SFW Hug GIF"
         try:
             hug_link = json.loads(main_api("hug"))["link"]
             return hug_link
         except:
-            api_down = "We might have a problem with the API, Please wait for it to be available"
             return api_down
 
     def smile():
+        "A Function That Will Return a SFW Smile GIF"
         try:
             smile_link = json.loads(main_api("smile"))["link"]
             return smile_link
         except:
-            api_down = "We might have a problem with the API, Please wait for it to be available"
             return api_down
 
     def headpat():
+        "A Function That Will Return a SFW Headpat GIF"
         try:
             headpat_link = json.loads(main_api("headpat"))["link"]
             return headpat_link
         except:
-            api_down = "We might have a problem with the API, Please wait for it to be available"
             return api_down
 
     def poke():  # Not Implemented Yet!
@@ -55,7 +53,6 @@ class sfw:
             poke_link = json.loads(main_api("poke"))["link"]
             return poke_link
         except:
-            api_down = "We might have a problem with the API, Please wait for it to be available"
             return api_down
 
     def bite():  # Not Implemented Yet!
@@ -63,7 +60,6 @@ class sfw:
             bite_link = json.loads(main_api("bite"))["link"]
             return bite_link
         except:
-            api_down = "We might have a problem with the API, Please wait for it to be available"
             return api_down
 
     def neko():  # Not Implemented Yet!
