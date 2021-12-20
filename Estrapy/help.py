f3fe7ae74f58d085ed96e0f7dda15e15d15c4572
unknown <damodaraswarupa@gmail.com>
2021-12-20 13:45:22 +0800

    Added Description and Example to Functions

--- Estrapy/help.py
+++ Estrapy/help.py
@@ -8,16 +8,64 @@ GAMES_LIST = ["truth", "dare"]
 class Help:
     @staticmethod
     def sfw():
+        """
+        Description
+        --------------
+        A Function That Will Return a List of Sfw Endpoints
+        
+        How to use sfw help function (Examples)
+        ----------------------------
+        
+        ```
+        Estrapy.Help.sfw() # Keep it as function or it will return function type
+        ```
+        """
         return SFW_LIST
 
     @staticmethod
     def nsfw():
+        """
+        Description
+        --------------
+        A Function That Will Return a List of Nsfw Endpoints
+        
+        How to use nsfw help function (Examples)
+        ----------------------------
+        
+        ```
+        Estrapy.Help.nsfw() # Keep it as function or it will return function type
+        ```
+        """
         return NSFW_LIST
 
     @staticmethod
     def games():
+        """
+        Description
+        --------------
+        A Function That Will Return a List of Games Endpoints
+        
+        How to use games help function (Examples)
+        ----------------------------
+        
+        ```
+        Estrapy.Help.games() # Keep it as function or it will return function type
+        ```
+        """
         return GAMES_LIST
 
     @staticmethod
     def all():
+        """
+        Description
+        --------------
+        A Function That Will Return a List of Sfw, Nsfw, Games Endpoints
+        
+        How to use all help function (Examples)
+        ----------------------------
+        
+        ```
+        Estrapy.Help.all() # Keep it as function or it will return function type
+        ```
+        """
         return SFW_LIST, NSFW_LIST, GAMES_LIST
