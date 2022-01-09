69f670dc2afd82a6a80ab86f812e5068a13d72b2
unknown <damodaraswarupa@gmail.com>
2022-01-09 11:08:38 +0800

    Updated AutoUpdate.py

--- Estrapy/autoupdate.py
+++ Estrapy/autoupdate.py
@@ -1,11 +1,12 @@
 import requests
 import os
+from __init__ import __version__ as current_version
 
 version = requests.get("https://estra-api.herokuapp.com/version").json()["Estrapy-API"]["Estrapy Version"]
 
 
 def autoupdate():
-    if version != "0.1.8":
+    if current_version != version:
         print(
             f"Estrapy-API - new version is up! AutoUpdate will be upgrading version to {version}"
         )
