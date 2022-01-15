57acf394bb5caac7978f2678d5c6f698f9d5f1cc
unknown <damodaraswarupa@gmail.com>
2022-01-15 14:00:32 +0800

    Updated AutoUpdate

--- Estrapy/autoupdate.py
+++ Estrapy/autoupdate.py
@@ -3,11 +3,11 @@ import requests
 import os
 from .__init__ import __version__ as current_version
 
-version = requests.get("https://estra-api.herokuapp.com/version").json()["Estrapy-API"][
+version = requests.get("https://estra-api.herokuapp.com/version/").json()["Estrapy-API"][
     "Estrapy Version"
 ]
-change_log = requests.get("https://estra-api.heroku.com/version").json()["Estra-API"][
-    "Estrapy Change Log"
+change_log = requests.get("https://estra-api.herokuapp.com/version/").json()["Estrapy-API"][
+    "Change Logs"
 ]
 
 
@@ -16,14 +16,24 @@ class AutoUpdate:
         self.output = output
         self.change_log = change_log
 
-    def settings(self):
+
+    def updater():
+        if current_version != version:
+            print(f"Estrapy-API - new version is up! AutoUpdate will be upgrading version to {version}")
+            os.system(f"pip install Estrapy-API=={version}")
+        print("Estrapy-API - No need to update")
+
+
+    def update(self):
+        if self.output and self.change_log:
+            AutoUpdate.updater()
+            print(f"Estrapy Changelog - {change_log}")
+            return
+
         if self.output:
-            if current_version != version:
-                print(
-                    f"Estrapy-API - new version is up! AutoUpdate will be upgrading version to {version}"
-                )
-                os.system(f"pip install Estrapy-API=={version}")
-            print("Estrapy-API - No need to update")
+            AutoUpdate.updater()
+            return
 
         if self.change_log:
-            print(f"Estrapy CHangelog - {change_log}")
+            print(f"Estrapy Changelog - {change_log}")
+            return
