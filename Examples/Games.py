a8f63efd7af9c7facbfbd9e92db04a4cd4903f2e
unknown <damodaraswarupa@gmail.com>
2022-01-30 11:30:52 +0800

    Updated Examples

--- Examples/Games.py
+++ Examples/Games.py
@@ -29,7 +29,8 @@ async def shipper():
         background="None",
         background_size="None",
     )
-    test.show()
+    test.show() # Showing the Image
+    test.save("Shipper.png") # Saving the Image
 
 
 asyncio.run(shipper())
