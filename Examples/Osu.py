6dcb4ff5dacb19f97847d6f9254651afc1dee3bf
unknown <damodaraswarupa@gmail.com>
2022-05-15 12:40:22 +0800

    Updated Examples

--- Examples/Osu.py
+++ Examples/Osu.py
@@ -4,7 +4,7 @@ import asyncio
 
 client_id = ""  # Put your own osu client_id
 client_secret = ""  # Put your own osu client_secret
-Osu = OsuClients(client_id=client_id, client_secret=client_secret).client_data()
+Osu = OsuClients(client_id=client_id, client_secret=client_secret)
 
 
 async def beatmap():
