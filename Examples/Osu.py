c641415bd5780360f3819b278996506291883e03
unknown <damodaraswarupa@gmail.com>
2022-05-17 21:24:39 +0800

    Updated Examples

--- Examples/Osu.py
+++ Examples/Osu.py
@@ -5,11 +5,14 @@ import asyncio
 client_id = ""  # Put your own osu client_id
 client_secret = ""  # Put your own osu client_secret
 Osu = OsuClients(client_id=client_id, client_secret=client_secret)
+OsuObject = OsuClients(
+    client_id=client_id, client_secret=client_secret, output="object"
+)
 
 
 async def beatmap():
-    data = await Estrapy.OsuClients.osubeatmap(beatmap_id="2405223")
-    data_formatter = await Estrapy.OsuClients.osubeatmap(
+    data = await Osu.osubeatmap(beatmap_id="2405223")
+    data_formatter = await Osu.osubeatmap(
         beatmap_id="2405223",
         formatter=True,  # Keep it on mind, this will only making the output with better formatting JSON format
     )
@@ -21,8 +24,8 @@ asyncio.run(beatmap())
 
 
 async def profile():
-    data = await Estrapy.OsuClients.osuprofile(username="Stawa")
-    data_formatter = await Estrapy.OsuClients.osuprofile(
+    data = await Osu.osuprofile(username="Stawa")
+    data_formatter = await Osu.osuprofile(
         username="Stawa",
         formatter=True,  # Keep it on mind, this will only making the output with better formatting JSON format
     )
@@ -31,3 +34,12 @@ async def profile():
 
 
 asyncio.run(profile())
+
+
+async def osu_object():
+    example = await OsuObject.osuprofile(username="Stawa")
+    print(example.avatar_url)
+    print(example.discord)
+
+
+asyncio.run(osu_object())
