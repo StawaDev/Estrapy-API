ad10ee344122dab7fe870782f50de4735befbeed
Stawa <77476824+StawaDev@users.noreply.github.com>
2022-10-03 19:22:23 +0800

    Add Requests Post

--- Estrapy/http.py
+++ Estrapy/http.py
@@ -21,3 +21,19 @@ def get_api(route: str) -> dict:
         raise InvalidResponse(f"Excected JSON from API but received {r.text}")
     except requests.exceptions.ConnectionError:
         raise APIOffline("The Estrapy API is offline.")
+
+
+def post_api(route: str, json: any) -> dict:
+    r = None
+
+    try:
+        r = requests.post(url=BASE_URL + route, json=json)
+
+        if not 200 <= r.status_code < 300:
+            raise InvalidStatusCode(f"{route} returned {r.status_code}")
+
+        return r.json()
+    except json.decoder.JSONDecodeError:
+        raise InvalidResponse(f"Excected JSON from API but received {r.text}")
+    except requests.exceptions.ConnectionError:
+        raise APIOffline("The Estrapy API is offline.")
