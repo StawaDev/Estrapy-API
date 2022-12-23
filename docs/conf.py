45539f8b43c32655d92dafb11e640af63a07b9c2
Stawa <77476824+StawaDev@users.noreply.github.com>
2022-12-23 19:14:27 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../"))
+
 project = "Estrapy"
 copyright = "2022, StawaDev"
 author = "StawaDev"
